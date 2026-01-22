1f2c09e1c1dd79f289b5f7000be64edd8c02dedc
Yash Mane <141533114+Ayankouji@users.noreply.github.com>
2026-01-22 13:01:01 +0530

    Update main.py with health check endpoint

--- app/main.py
+++ app/main.py
@@ -22,7 +22,6 @@
 #     clean_data = clean_invoice_data(raw_data)
 
 #     return clean_data
-
 from fastapi import FastAPI, UploadFile, File
 import shutil
 import os
@@ -36,13 +35,21 @@ app = FastAPI(title="Invoice OCR Minimal API")
 UPLOAD_DIR = "app/storage/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+# Render health check
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
 @app.post("/extract-invoice", response_model=InvoiceMinimalResponse)
 async def extract_invoice(file: UploadFile = File(...)):
-    file_path = f"{UPLOAD_DIR}/{file.filename}"
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     raw_data = process_invoice(file_path)
+
+    # delete file after processing
+    os.remove(file_path)
 
     return extract_invoice_minimal(raw_data)
